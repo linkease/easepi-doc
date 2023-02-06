a568d2140c76dc289b71f98f9e63a2ec595bd56e
t-xiaodan <1609910273@qq.com>
2023-02-06 14:44:55 +0800

    cpmmit update Copyright

--- docs/.vuepress/components/FooterComponent.vue
+++ docs/.vuepress/components/FooterComponent.vue
@@ -3,17 +3,19 @@
         <div class="footer-content">
             <div class="content_text">
                 <div class="content_block">
-                    <template v-for="(item,i) in menus">
+                    <template v-for="(item, i) in menus">
                         <ul class="content_list" :key="i">
-                            <li class="content_title" :title="item.title">{{item.title}}</li>
-                            <template v-for="(ole,k) in item.childer">
-                                <li :key="k"><a :href="ole.value" :title="ole.label" target="_blank">{{ole.label}}</a></li>
+                            <li class="content_title" :title="item.title">{{ item.title }}</li>
+                            <template v-for="(ole, k) in item.childer">
+                                <li :key="k"><a :href="ole.value" :title="ole.label" target="_blank">{{ ole.label }}</a>
+                                </li>
                             </template>
                         </ul>
                     </template>
                 </div>
             </div>
-            <div class="content_info" title="易有云网络科技有限责任公司 Copyright © 2019-2021  备案号： 粤ICP备20024441号">易有云网络科技有限责任公司 Copyright © 2019-2021<br />
+            <div class="content_info" title="易有云网络科技有限责任公司 Copyright © 2019-2023  备案号： 粤ICP备20024441号">易有云网络科技有限责任公司
+                Copyright © 2019-2023<br />
                 <a href="https://beian.miit.gov.cn/#/Integrated/index" target="_blank">备案号： 粤ICP备20024441号</a>
             </div>
         </div>
@@ -105,6 +107,7 @@ export default {
     word-wrap: break-word;
     outline: none;
 }
+
 .footer {
     // background-color: #f4f4f4;
     min-height: 234px;
@@ -113,9 +116,11 @@ export default {
     align-items: center;
     flex-wrap: wrap;
     background: #000;
+
     .footer-content {
         display: flex;
         flex-wrap: wrap;
+
         .content_text {
             width: 1138px;
             display: flex;
@@ -127,14 +132,17 @@ export default {
             border-top: 1.5px solid rgba(0, 0, 0, 0.7);
             padding-top: 24px;
             padding-bottom: 24px;
+
             .content_block {
                 display: flex;
                 justify-content: space-between;
                 flex-wrap: wrap;
+
                 // width: 1020px;
                 .content_list {
                     margin: 0;
                     padding: 10px 50px;
+
                     .content_title {
                         font-size: 16px;
                         font-family: PingFangSC-Semibold, PingFang SC;
@@ -143,6 +151,7 @@ export default {
                         line-height: 22px;
                         padding-bottom: 12px;
                     }
+
                     a {
                         display: block;
                         font-size: 14px;
@@ -152,26 +161,31 @@ export default {
                         line-height: 20px;
                         margin-top: 8px;
                     }
+
                     a:hover {
                         color: #0060ff;
                     }
                 }
             }
         }
+
         .content_info {
             flex: 0 0 100%;
             font-size: 12px;
             color: #8e8e8e;
             padding: 0 50px;
+
             a {
                 color: #8e8e8e;
             }
+
             a:hover {
                 color: #0060ff;
             }
         }
     }
 }
+
 li {
     list-style: none;
 }
@@ -182,10 +196,12 @@ li {
         .footer-content {
             .content_text {
                 width: 100%;
+
                 .content_block {
                     max-width: 100%;
                     flex-wrap: nowrap;
                     padding: 0;
+
                     .content_list {
                         flex: 0 0 100%;
                         max-width: 25%;
@@ -198,15 +214,18 @@ li {
         }
     }
 }
+
 @media screen and (max-width: 1300px) {
     .footer {
         .footer-content {
             .content_text {
                 width: 100%;
+
                 .content_block {
                     max-width: 100%;
                     flex-wrap: nowrap;
                     padding: 0 30px;
+
                     .content_list {
                         flex: 0 0 100%;
                         max-width: 25%;
@@ -216,67 +235,79 @@ li {
                     }
                 }
             }
+
             .content_info {
                 padding-left: 30px;
             }
         }
     }
 }
+
 @media screen and (max-width: 1100px) {
     .footer {
         .footer-content {
             .content_text {
                 width: 100%;
+
                 // margin: 0 80px;
                 .content_block {
                     max-width: 100%;
                     flex-wrap: wrap;
                     padding: 0 30px;
+
                     .content_list {
                         flex: 0 0 100%;
                         max-width: 25%;
                         margin: 0;
                         padding: 0;
                         margin-top: 10px;
+
                         .content_title {
                             padding-bottom: 0px;
                         }
                     }
                 }
             }
+
             .content_info {
                 padding-left: 30px;
             }
         }
     }
 }
+
 @media screen and(max-width:1024px) {
     .footer {
         .footer-content {
             .content_text {
                 width: 100%;
+
                 .content_block {
                     max-width: 100%;
                     flex-wrap: wrap;
                     padding: 0 80px;
+
                     .content_list {
                         flex: 0 0 100%;
                         max-width: 25%;
                         margin: 0;
                         padding: 0;
                         margin-top: 12px;
+
                         .content_title {
                             padding-bottom: 6px;
                         }
                     }
                 }
             }
+
             .content_info {
                 padding-left: 80px;
             }
         }
     }
 }
+
 @media screen and (max-width: 900px) {
     .footer {
         .footer-content {
@@ -284,22 +315,26 @@ li {
                 width: 100%;
                 padding-bottom: 0px;
                 padding-top: 16px;
+
                 .content_block {
                     width: 100%;
                     padding: 0 10px;
                     flex-wrap: wrap;
+
                     .content_list {
                         flex: 0 0 100%;
                         max-width: 50%;
                         margin: 0;
                         padding: 0;
                         margin-bottom: 20px;
+
                         .content_title {
                             padding-bottom: 0px;
                         }
                     }
                 }
             }
+
             .content_info {
                 padding-left: 10px;
             }
